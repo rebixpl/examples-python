8117a3e849b57ef679c5372b774c2a9b59431918
rebixpl <33410494+rebixpl@users.noreply.github.com>
2019-12-23 09:04:43 +0100

    Create comments_variables_turtle.py

---
+++ comments_variables_turtle.py
@@ -0,0 +1,39 @@
+import turtle
+
+a = 5
+b = "cool"
+b2 = "very"
+c = b2 + " " + b
+print('Python is ' + b,a)
+print(c)
+
+x = "John"
+#is the same as
+x = 'John'
+
+x,y,z = "orange", "banana", "cherry"
+print(x,y,z)
+
+credit_card = 592375982375923759832757239
+print(credit_card)
+
+#creating a turtle which you can move, YOU NEED TO IMPORT turtle LIBRARY
+qazi_turtle = turtle.Turtle()
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+qazi_turtle.right(90)
+qazi_turtle.forward(100)
+
+#prvevent window from closing
+PAUSE = input()
+
+#this in one line comment in python
+
+"""
+this is multiple
+line comment in
+python
+"""
