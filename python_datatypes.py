c8909a177ba66f566cb314907192c2344e804d75
rebixpl <33410494+rebixpl@users.noreply.github.com>
2019-12-27 11:26:44 +0100

    Create python_datatypes.py

---
+++ python_datatypes.py
@@ -0,0 +1,32 @@
+
+# Integers
+i = 5
+
+# Floats
+f = -5.42342
+
+# Strings
+strDouble = "yo hows it going"
+strSingle = 'this is string too'
+
+# Boolens
+bT = True
+bF = False
+
+# Complex
+c = 1j
+
+# List
+myList = ["apple", "banana", "cherry"]
+
+# Tuple
+myTuple = ("apple", "banana", "cherry")
+
+# Disctionary
+myDictionary = {"car" : "tesla", "language" : "english", "age" : 15}
+
+# Set
+mySet = {"apple", "banana", "cherry"}
+
+# You can check the type of variable by using type()
+print(type(i), type(f), type(strDouble), type(bT), type(c), type(myList), type(myTuple), type(myDictionary), type(mySet))
