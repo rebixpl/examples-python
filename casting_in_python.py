609dec477c6df7a4cc3001504b8fe3eb6833594c
rebixpl <33410494+rebixpl@users.noreply.github.com>
2019-12-27 11:38:51 +0100

    Create casting_in_python.py

---
+++ casting_in_python.py
@@ -0,0 +1,16 @@
+#You can cast variables by using this functions:
+# int(), bool(), str(), float()
+
+print(int(3.4242)) # float -> int
+print(float("6.232")) # str -> float
+
+thisIsString = "4004"
+print(int(thisIsString)) # str -> int
+print(type(int(thisIsString)))
+
+print(bool(1)) # True - 1 or other value # int -> bool
+print(bool(0)) # False - 0
+
+print(str(424))
+print(type(str(424))) # int -> str
+
