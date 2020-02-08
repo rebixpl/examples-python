4a37301ae0792461c6149e95df20d27959dd8ebd
rebixpl <33410494+rebixpl@users.noreply.github.com>
2020-02-08 12:23:23 +0100

    Create python_user_input.py

---
+++ basics/python_user_input.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+#!  Python User Input
+
+# python3 uses input() method
+username = input("Enter username: ")
+print("Username is: " + username)
+
+
+
+# python2 uses raw_input() method
+username = raw_input("Enter username: ")
+print("Username is: " + username)
