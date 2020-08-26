b4eb5eebd6212d94ba1b569ede1cfe18eb1a2b8f
rebixpl <rebixcraft@gmail.com>
2020-08-26 06:49:28 +0200

    tuples, sets

--- python study for machine learning/tuples_sets.py
+++ python study for machine learning/tuples_sets.py
@@ -96,3 +96,91 @@ print(x)  # OUTPUT: 6
 thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5, 6, 3, 8, 3, 5, 5, 5, 2, 5, 2, 3, 6)
 x = thistuple.index(7)
 print(x)  # OUTPUT: 2
+
+# SETS #####################################################################
+
+# In Python sets are written with curly brackets. Sets are unordered,
+# so you cannot be sure in which order the items will appear.
+
+thisisset = {"tomato", "car", "machine learning", "gpt-3"}
+print(thisisset)  # OUTPUT:
+
+# Access Items
+# Of course, you CANNOT access items by index, but you can use for loop
+# or check if value is present in set:
+
+thisisset = {"tomato", "car", "machine learning", "gpt-3"}
+for item in thisisset:
+    print(item)
+
+# OUTPUT:
+# car
+# machine learning
+# tomato
+# gpt-3
+
+# Check if "car" is present in the set:
+print("car" in thisisset)  # OUTPUT: True
+
+# Change Items ##
+# You CANNOT change items in set after its creation, but you can add
+# new items.
+
+# add() → add one item
+thisset = {"apple", "banana", "cherry"}
+thisset.add("blueberry")
+
+print(thisset)  # OUTPUT: {'blueberry', 'apple', 'cherry', 'banana'}
+
+
+# update() → add more than one item
+thisset = {"apple", "banana", "cherry"}
+thisset.update(["grapes", "mango", "dragonfruit", "kiwi"])
+# OUTPUT: {'kiwi', 'banana', 'mango', 'grapes', 'dragonfruit', 'cherry', 'apple'}
+print(thisset)
+
+# Remove Item
+
+# remove() -> If the item to remove does not exist, remove() will raise
+# an error.
+
+thisset = {"a", "b", "c", "d", "e"}
+thisset.remove("b")
+print(thisset)  # OUTPUT: {'a', 'd', 'e', 'c'}
+
+# discard() ->  If the item to remove does not exist, discard() will
+# NOT raise an error.
+
+thisset = {"a", "b", "c", "d", "e"}
+thisset.discard("b")
+print(thisset)  # OUTPUT: {'a', 'd', 'e', 'c'}
+
+# pop() -> Sets are unordered, thus it deletes random item
+
+# clear() -> empties the set
+thisset = {"a", "b", "c", "d", "e"}
+thisset.clear()
+print(thisset)  # OUTPUT: set()
+
+# del keyword → deletes the set completely
+thisset = {"a", "b", "c", "d", "e"}
+del thisset
+
+# Join Two Sets
+# union() → returns a new set with all items from both sets:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)  # OUTPUT: {'a', 1, 2, 3, 'c', 'b'}
+
+# update() → inserts the items in set2 into set1:
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set1.update(set2)
+print(set1)  # OUTPUT: {'a', 1, 2, 3, 'c', 'b'}
+
+# The set() constructor
+myset = set(("apple", "africa", "the planet of death"))
+print(myset)  # OUTPUT: {'the planet of death', 'apple', 'africa'}
